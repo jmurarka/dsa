15fffae7381de92b189ce31c38a474fc201321d3
Jhanvi Murarka <jhanvimurarka@gmail.com>
2026-06-22 20:07:58 +0530

    Time: 0 ms (100%), Space: 16.4 MB (75.46%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -4,9 +4,31 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        res = 0
+        st = 0
+        end = len(nums) - 1
 
-        for num in nums:
-            res ^= num
+        if len(nums) == 1:
+            return nums[0]
 
-        return res
+        while st <= end:
+            mid = (end+st) // 2
+
+            if mid + 1 <= end and nums[mid] == nums[mid+1]:
+                if mid % 2 == 0:
+                    st = mid + 1
+
+                else:
+                    end = mid - 1
+
+            elif mid - 1 >= 0 and nums[mid] == nums[mid-1]:
+                if mid %2 != 0:
+                    st = mid + 1
+
+                else:
+                    end = mid - 1
+
+            else:
+                return nums[mid]
+
+
+        return nums[0]
