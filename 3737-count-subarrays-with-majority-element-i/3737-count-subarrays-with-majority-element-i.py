7571c09078aeb807b5ebda921bcb898e1e2e84e9
Jhanvi Murarka <jhanvimurarka@gmail.com>
2026-06-25 22:01:06 +0530

    Time: 25 ms (93.02%), Space: 12.6 MB (41.86%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.py
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.py
@@ -9,15 +9,33 @@ class Solution(object):
         cnt = 0
 
         for i in range(n):
-            freq = 0
+            if nums[i] == target:
+                nums[i] = 1
+            else:
+                nums[i] = -1
 
-            for j in range(i, n):
-                if nums[j] == target:
-                    freq += 1
+        pref = [0] * n
+        pref[0] = nums[0]
 
-                length = j - i + 1
+        for i in range(1, n):
+            pref[i] = pref[i - 1] + nums[i]
 
-                if freq > length // 2:
-                    cnt += 1
+        shift = n
+        freq = [0] * (2 * n + 1)
+
+        freq[shift] = 1
+
+        valid = 0
+        lastSum = 0
+
+        for i in range(n):
+            if pref[i] > lastSum:
+                valid += freq[lastSum + shift]
+            else:
+                valid -= freq[pref[i] + shift]
+
+            cnt += valid
+            freq[pref[i] + shift] += 1
+            lastSum = pref[i]
 
         return cnt
