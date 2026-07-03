666f6e2a02f68b25227c3af648b2d564dffadc86
Jhanvi Murarka <jhanvimurarka@gmail.com>
2026-07-03 16:27:26 +0530

    Time: 2789 ms (9.09%), Space: 50.8 MB (54.55%) - LeetHub

---
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.py
@@ -0,0 +1,79 @@
+class Solution(object):
+    def findMaxPathScore(self, edges, online, k):
+        """
+        :type edges: List[List[int]]
+        :type online: List[bool]
+        :type k: int
+        :rtype: int
+        """
+        def check(mid,adj,topo,online,k,n):
+            dist = [float('inf')] * n
+
+            dist[0] = 0
+
+            for u in topo:
+                if dist[u] == float('inf'):
+                    continue
+
+                if u != 0 and u != n-1 and not online[u]:
+                    continue
+
+
+                for v,w in adj[u]:
+                    if w < mid:
+                        continue
+
+
+                    if v!= n-1 and not online[v]:
+                        continue
+
+                    dist[v] = min(dist[v], dist[u] + w)
+
+            return dist[n-1] <= k
+
+        n = len(online)
+
+        adj = [[] for _ in range(n)]
+
+        indegree = [0] * n
+
+        max_edge = 0
+
+        for u,v,w in edges:
+            adj[u].append((v,w))
+            indegree[v] += 1
+
+            max_edge = max(max_edge,w)
+
+        q = deque()
+
+        for i in range(n):
+            if indegree[i] == 0:
+                q.append(i)
+
+        topo = []
+
+        while q:
+            u = q.popleft()
+            topo.append(u)
+
+            for v,_ in adj[u]:
+                indegree[v] -= 1
+                if indegree[v] == 0:
+                    q.append(v)
+
+        low,high = 0, max_edge
+
+        ans = -1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if check(mid,adj,topo,online,k,n):
+                ans = mid
+                low = mid + 1
+
+            else:
+                high = mid - 1
+
+        return ans 
