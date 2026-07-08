c2a41418eb9ea9ba805fa31fb145336707e80e09
Jhanvi Murarka <jhanvimurarka@gmail.com>
2026-07-08 12:19:14 +0530

    Time: 10 ms (9.78%), Space: 12.2 MB (84.78%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -4,26 +4,6 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        stck = []
-
-        while n > 0:
-            rem = n % 10
-
-            if rem != 0:
-                stck.append(rem)
-
-            n = n //10
-
-        stck.reverse()
-
-        x = 0
-
-        for s in stck:
-            x = x* 10 + s
-
-
-        s = sum(stck)
-
-
-        return x * s
-        
+        digits = [int(c) for c in str(n) if c != '0']
+        x = int(''.join(map(str, digits))) if digits else 0
+        return x * sum(digits)
